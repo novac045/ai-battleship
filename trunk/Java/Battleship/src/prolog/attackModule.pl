f9893b1018d095d358413ad38b2c7c1790b8ed71
Victor.Apostel@gmail.com <Victor.Apostel@gmail.com@817caa91-668a-c7fb-5c67-0c7aa5277178>
2011-01-05 13:06:40 +0000

    Doppelte Koordinaten werden geprüft

--- trunk/Java/Battleship/src/prolog/attackModule.pl
+++ trunk/Java/Battleship/src/prolog/attackModule.pl
@@ -5,10 +5,28 @@
 
 /* praedikat doAttack(X, Y) */
 doAttack(X, Y) :-
-	randValue(X),
-	randValue(Y),
+	randValue(A),
+	randValue(B),
+        /*write('Random: '), write(A), write(','), write(B), nl,*/
+	doAttackCheck(A, B, XVerified, YVerified),
+        X is XVerified,
+        Y is YVerified,
+        !.
+
+doAttackCheck(X, Y, XVerified, YVerified) :-
 	enemyField(Field),
-	member(X/Y/0, Field), !.
+	member(X/Y/0, Field),
+        XVerified is X,
+        YVerified is Y,
+        !.
+
+doAttackCheck(X, Y, XVerified, YVerified) :-
+	enemyField(Field),
+	\+ member(X/Y/0, Field),
+        doAttack(A, B),
+        XVerified is A,
+        YVerified is B,
+        !.
 
 /* praedikat attackResponse( State ) */
 attackResponse(X, Y, State) :-
