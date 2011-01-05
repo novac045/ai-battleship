635ae2cee0f5140e6f5936c5b7bc3c2562c1b314
sibilleritter@gmx.de <sibilleritter@gmx.de@817caa91-668a-c7fb-5c67-0c7aa5277178>
2011-01-05 14:51:30 +0000

    check for completely destroyed

--- trunk/Java/Battleship/src/prolog/defendModule.pl
+++ trunk/Java/Battleship/src/prolog/defendModule.pl
@@ -1,36 +1,79 @@
 
-doDefend(X, Y, RetVal) :-
+/* ---------------------------------------------- */	
+doDefend(X, Y, NewState) :-
 	myField(MyField),
 	member(X/Y/State, MyField), 
 	/* update Field for hit at X/Y */
-	updateMyField(X, Y, State),
-	returnValue(X, Y, State, MyField, RetVal), !.
+	updateMyField(X, Y, State, NewState),
+	!.
 
-/* water - nothing to do */
-updateMyField(_, _, 1).
+/* ---------------------------------------------- */	
+/* water was hit - nothing to do */
+/*               - state MISSED??! */
+updateMyField(_, _, 1, 1).
 
-/* ship - update ship */
-updateMyField(X, Y, 6) :-
+/* ship was hit - state hit:       2 */
+updateMyField(X, Y, 6, NewState) :-
 	myField(OldField),
 	substitute(X/Y/6, OldField, X/Y/2, NewField),
 	retractall(myField(_)),
-	asserta(myField(NewField)).
+	asserta(myField(NewField)),
+	updateShips(X, Y, NewState).
+
+/* ---------------------------------------------- */	
+/* ship was hit - state last ship destroyed: 4    */
+/*              - if no state 6 (ship) in myField */
+updateShips(_, _, 4) :-
+	myField(MyField),
+	\+ member(_/_/6, MyField).
+
+/* ship was hit - state whole destroyed: 3 */
+updateShips(X, Y, 3) :-
+	completelyDestroyed(X, Y, X, Y).
 	
-/* water */
-returnValue(_, _, 1, _, 1). 
+/* else         - just a normal hit */
+updateShips(_, _, 2).
 
-/* lastShipDestroyed - if no State=6 (ship) in myField */
-returnValue(_, _, 6, MyField, 4) :-
-	\+ member(_/_/6, MyField). 
+/* ---------------------------------------------- */	
+/* do not regard field borders                    */
+completelyDestroyed(X, Y, _, _) :-
+	(X < 0; X >= 10),
+	(Y < 0; Y >= 10).
 
+/* not destroyed if there is State SHIP: 6        */
+completelyDestroyed(X, Y, _, _) :-
+	myField(MyField),
+	member(X/Y/6, MyField),
+	!,
+	fail.
 
-/* destroyed */
-/* returnValue(X, Y, 6, _, 3). */
+/* recursion if state is HIT: 2                   */
+completelyDestroyed(X, Y, OldX, OldY) :-
+	myField(MyField),
+	member(X/Y/2, MyField),
+	
+	east(X, Y, OldX, OldY),
+	west(X, Y, OldX, OldY),
+	north(X, Y, OldX, OldY),
+	south(X, Y, OldX, OldY).
+	
+	
+east(X, _, X, _).
+east(X, Y, OldX, OldY) :-
+	NewX is X+1,
+	completelyDestroyed(NewX, Y, X, OldY).
 
-/* hit */
-returnValue(_, _, 6, _, 2).
+west(X, _, X, _).
+west(X, Y, OldX, OldY) :-
+	NewX is X-1,
+	completelyDestroyed(NewX, Y, X, OldY).
 
-/* error, e.g. if same ship-spot was hit twice */
-/* 0 - unknown!!! */
-/*retrunValue(_, _, _, _, 0).*/
-
+north(_, Y, _, Y).
+north( X, Y, OldX, OldY) :-
+	NewY is Y-1,
+	completelyDestroyed(X, NewY, OldX, Y).
+	
+south(_, Y, _, Y).
+south( X, Y, OldX, OldY) :-
+	NewY is Y+1,
+	completelyDestroyed(X, NewY, OldX, Y).
