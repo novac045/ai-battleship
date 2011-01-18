27725d5a3e7a44abef26f16257e9738a94aa5550
Victor.Apostel@gmail.com <Victor.Apostel@gmail.com@817caa91-668a-c7fb-5c67-0c7aa5277178>
2011-01-18 19:19:01 +0000

    Argumente koennen der GUI beim Start uebergeben werden -pPORT -hHOST

--- trunk/Java/Battleship/src/battleship/GuiMain.java
+++ trunk/Java/Battleship/src/battleship/GuiMain.java
@@ -13,7 +13,20 @@ public class GuiMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        CPlayingFieldController control = new CPlayingFieldController(10, 10, "127.0.0.1", 54321);
+        String host = "127.0.0.1";
+        int port = 54321;
+        for (String arg : args) {
+            if (arg.contains("-h")) {
+                String h = arg.replaceAll("-h", "");
+                host = h;
+            } else if (arg.contains("-p")) {
+                String p = arg.replaceAll("-p", "");
+                port = Integer.parseInt(p);
+            }
+        }
+        System.out.println(host);
+        System.out.println(port);
+        CPlayingFieldController control = new CPlayingFieldController(10, 10, host, port);
         CBattleshipGUI gui = new CBattleshipGUI(control);
         Thread mainThread = new Thread(control);
         mainThread.start();
