93564814b954c6f9193a40d1fd6487b511eb4205
stefanbogi <stefanbogi@817caa91-668a-c7fb-5c67-0c7aa5277178>
2011-01-18 10:42:53 +0000

    Prolog Module kommentiert, Präsentation fortgeführt

--- Java/Battleship/src/prolog/initShips.pl
+++ Java/Battleship/src/prolog/initShips.pl
@@ -4,66 +4,67 @@ Verwendeten Schiffe.
 Schiffs-ID / Teilenummer / X-Koordinate / Y-Koordinate*/
 :-assert(template(
 [
+	/*5er Schiff*/
 	1/5/_X11/_Y11,
 	1/4/_X12/_Y12,
 	1/3/_X13/_Y13,
 	1/2/_X14/_Y14,
 	1/1/_X15/_Y15,
-
+	/*4er Schiff*/
 	2/4/_X21/_Y21,
 	2/3/_X22/_Y22,
 	2/2/_X23/_Y23,
 	2/1/_X24/_Y24,
-
+	/*3er Schiff*/
 	3/3/_X31/_Y31,
 	3/2/_X32/_Y32,
 	3/1/_X33/_Y33,
-
+	/*3er Schiff*/
 	4/3/_X41/_Y41,
 	4/2/_X42/_Y42,
 	4/1/_X43/_Y43,
-
+	/*2er Schiff*/
 	5/2/_X51/_Y51,
 	5/1/_X52/_Y52])
 ).
 /* Teile des selben Schiffes müssen 
 aneinander liegen (Horizontal) */
 connectedHV(S1,P1,X1,Y1,S2,P2,X2,Y2):-
-	S1 == S2,
-	P1 \== P2,
-	X1-X2 =:= P1-P2,
-	Y1 == Y2,
+	S1 == S2,		%Das Selbe Schiff,
+	P1 \== P2,		%verschiedene Teile dieses Schiffs:
+	X1-X2 =:= P1-P2,%Aufeinander folgende teile, 
+	Y1 == Y2,		%müssen aufeinander folgenden Koordinaten liegen (Horizontal)
 	!
 .
 /* Teile des selben Schiffes müssen 
 aneinander liegen (Vertikal) */
 connectedHV(S1,P1,X1,Y1,S2,P2,X2,Y2):-
-	S1 == S2,
-	P1 \== P2,
-	Y1-Y2 =:= P1-P2,
-	X1 == X2,
+	S1 == S2,		%Das Selbe Schiff,
+	P1 \== P2,		%verschiedene Teile dieses Schiffs:
+	Y1-Y2 =:= P1-P2,%Aufeinander folgende teile, 
+	X1 == X2,		%müssen aufeinander folgenden Koordinaten liegen (Vertikal)
 	!
 .
 /*Ungleiche Schiffe müssen 
 Ein Feld Abstand halten, 
 ODER Diagonal versetzt stehen (Vertikal)*/
 distance(S1,S2,X1,Y1,X2,Y2):-
-	S1 \== S2,
-	((Y1 == Y2,
-	abs(X1-X2)>1);
-	(Y1 \== Y2,
-	abs(X1-X2)>=1)),
+	S1 \== S2,		%Verschiedene Schiffe:
+	((Y1 == Y2,		%bei gleicher Y-Koordinate
+	abs(X1-X2)>1);	%müssen die X-Koordinaten mehr als 1 Feld Abstand halten;
+	(Y1 \== Y2,		%bei verschiedener X-Koordinate
+	abs(X1-X2)>=1)),%reicht auch ein Feld Abstand (Diagonal versetzt)
 	!
 .
 /*Ungleiche Schiffe müssen 
 Ein Feld Abstand halten, 
 ODER Diagonal versetzt stehen (Horizontal)*/
 distance(S1,S2,X1,Y1,X2,Y2):-
-	S1 \== S2,
-	((X1 == X2,
-	abs(Y1-Y2)>1);
-	(X1 \== X2,
-	abs(Y1-Y2)>=1)),
+	S1 \== S2,		%Verschiedene Schiffe:
+	((X1 == X2,		%bei gleicher X-Koordinate
+	abs(Y1-Y2)>1);	%müssen die Y-Koordinaten mehr als 1 Feld Abstand halten;
+	(X1 \== X2,		%bei verschiedener Y-Koordinate
+	abs(Y1-Y2)>=1)),%reicht auch ein Feld Abstand (Diagonal versetzt)
 	!
 .
 /*Wendet die Platzierungsregeln für eine 
